341398ddc93670410bdc178d19d4575c93f6c9de
hyujikoh <dvum0045@gmail.com>
2023-06-25 17:23:53 +0900

    중앙화된 처리를 하기 위해 프론트 controller 추가 및 적용

--- src/main/java/com/inflearn/toby/helloproject/HelloProjectApplication.java
+++ src/main/java/com/inflearn/toby/helloproject/HelloProjectApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -24,16 +25,21 @@ public class HelloProjectApplication {
         // 추상화를 진행해서 다른 웹 서버를 사용할 수 있게 설정해야한다.
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("hello", new HttpServlet() {
+            servletContext.addServlet("frontcontroller", new HttpServlet() {
                 @Override
                 protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-                    String name = req.getParameter("name");
+                    // 인증, 보안, 다국어, 공통 기능을 처리
+                    if(req.getRequestURI().equals("/hello")&&req.getMethod().equals(HttpMethod.GET.name())){
+                        String name = req.getParameter("name");
 
-                    res.setStatus(HttpStatus.OK.value());
-                    res.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-                    res.getWriter().print("Hello Servlet" + name);
+                        res.setStatus(HttpStatus.OK.value());
+                        res.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                        res.getWriter().print("Hello Servlet" + name);
+                    }
+                    else if (req.getRequestURI().equals("/user")){}
+                    else res.setStatus(HttpStatus.NOT_FOUND.value());
                 }
-            }).addMapping("/hello");
+            }).addMapping("/*"); // 해당 요청을 모두 처리하겠다.
         });
         webServer.start();
     }
