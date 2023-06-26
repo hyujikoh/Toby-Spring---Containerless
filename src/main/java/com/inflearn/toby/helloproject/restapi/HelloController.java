fc4ec5fbe747f1c3f1520344ef3195fd13db3dce
hyujikoh <dvum0045@gmail.com>
2023-06-26 11:47:50 +0900

    helloController 와 SimpleHelloService 로직 분리

--- src/main/java/com/inflearn/toby/helloproject/restapi/HelloController.java
+++ src/main/java/com/inflearn/toby/helloproject/restapi/HelloController.java
@@ -1,8 +1,13 @@
 package com.inflearn.toby.helloproject.restapi;
 
+import java.util.Objects;
+
 public class HelloController {
 
     public String hello(String name){
-        return "hello world" + name;
+        SimpleHelloService simpleHelloService = new SimpleHelloService();
+
+        // 객체에 대해 notnull 로 처리
+        return simpleHelloService.sayHello(Objects.requireNonNull(name));
     }
 }
---
+++ src/main/java/com/inflearn/toby/helloproject/restapi/SimpleHelloService.java
@@ -0,0 +1,8 @@
+package com.inflearn.toby.helloproject.restapi;
+
+public class SimpleHelloService {
+    String sayHello(String name){
+
+        return "hello" + name;
+    }
+}
