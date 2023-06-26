f37f7068afbfcca737056b7e22f7fe3e802d3a15
hyujikoh <dvum0045@gmail.com>
2023-06-26 13:20:57 +0900

    어노테이션 적용

--- src/main/java/com/inflearn/toby/helloproject/restapi/HelloController.java
+++ src/main/java/com/inflearn/toby/helloproject/restapi/HelloController.java
@@ -1,7 +1,13 @@
 package com.inflearn.toby.helloproject.restapi;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.Objects;
 
+@RequestMapping("/hello")
 public class HelloController {
     // final 을 사용할때 초기화를 해야한다.
     private final HelloService helloService;
@@ -11,6 +17,8 @@ public class HelloController {
     }
 
 
+    @GetMapping
+    @ResponseBody
     public String hello(String name){
         // 객체에 대해 notnull 로 처리
         return helloService.sayHello(Objects.requireNonNull(name));
