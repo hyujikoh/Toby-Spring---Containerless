6ea1b057c28307dda4effd478206284d488fc8ec
hyujikoh <dvum0045@gmail.com>
2023-07-06 13:57:15 +0900

    바인드 방식으로 처리

--- src/main/java/com/inflearn/config/autoconfig/ServerPropertiesConfig.java
+++ src/main/java/com/inflearn/config/autoconfig/ServerPropertiesConfig.java
@@ -1,6 +1,7 @@
 package com.inflearn.config.autoconfig;
 
 import com.inflearn.config.MyAutoConfiguration;
+import org.springframework.boot.context.properties.bind.Binder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 
@@ -9,11 +10,13 @@ public class ServerPropertiesConfig {
 
     @Bean
     public ServerProperties serverProperties(Environment env){
-        ServerProperties properties = new ServerProperties();
+        // 기존에 필드를 작성하고 번거로운 과정을 bind 를 통해 알아서 연결해주는 방식으로 수정
+        return Binder.get(env).bind("",ServerProperties.class).get();
 
-        properties.setContextPath(env.getProperty("contextPath"));
-        properties.setPort(Integer.parseInt(env.getProperty("port")));
-
-        return properties;
+//        ServerProperties properties = new ServerProperties();
+//        properties.setContextPath(env.getProperty("contextPath"));
+//        properties.setPort(Integer.parseInt(env.getProperty("port")));
+//
+//        return properties;
     }
 }
