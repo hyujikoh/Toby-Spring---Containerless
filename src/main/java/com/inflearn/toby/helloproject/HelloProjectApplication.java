7ee8d9d28bf1553b878825794b5f2a40c5108b75
hyujikoh <dvum0045@gmail.com>
2023-07-06 11:37:53 +0900

    properties 안에 데이터를 가져와서 applicationRunner 를 통해 메서드 실행

--- src/main/java/com/inflearn/toby/helloproject/HelloProjectApplication.java
+++ src/main/java/com/inflearn/toby/helloproject/HelloProjectApplication.java
@@ -1,12 +1,24 @@
 package com.inflearn.toby.helloproject;
 
 import com.inflearn.config.MySpringBootApplication;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 import static org.springframework.boot.SpringApplication.run;
 
 @MySpringBootApplication
 public class HelloProjectApplication {
+
+    @Bean
+    ApplicationRunner applicationRunner(Environment env){
+        return args -> {
+            String name = env.getProperty("my.name");
+            System.out.println("my.name" + name);
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(HelloProjectApplication.class, args);
